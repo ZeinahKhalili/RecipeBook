2c5d3e21a4f414e0fafc15595bd5519710dc1ad8
ZeinahKhalili <zeinahkh98@gmail.com>
2020-10-11 15:42:03 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import {RecipeService} from './recipes/recipe.service';
 
 @Component({
   selector: 'rb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  providers: [RecipeService]
 })
 export class AppComponent {
-  title = 'recipe-book';
 }
